b2dea6baba42b63ac26c92a32c08796e81ebda1f
kirkdshelton <sheltk68@gmail.com>
2017-03-26 14:11:25 -0400

    Added Comment Features

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,5 @@
 class Article < ApplicationRecord
 
-	has_attached_file :image,
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-                      
+	has_many :comments
 	
 end
